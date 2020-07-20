dda1e7d9816384ed4bb39c02adf70f9e801e7fbd
AliceRez <Reznickova.alice@seznam.cz>
2020-07-20 10:33:06 +0200

    comits

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-var attempt = 0;
+var attempt = 1;
 var a, b;
+var storeValue;
 function turn(x, element) {
-  document.getElementById(`card${x}`).style.transform = "rotateY(180deg)";
-  attempt++;
+  element.childNodes[1].style.transform = "rotateY(180deg)";
   if (attempt == 1) {
     a = element.dataset.name;
+    storeValue = element.childNodes[1].id;
   } else if (attempt == 2) {
-    attempt = 0;
     b = element.dataset.name;
-    if (a == b) {
-      document.getElementById(`card${x}`).style.transform = "";
-    } else {
-      document.getElementById(`card${x}`).style.transform = "rotateY(0deg)";
+    if (a != b) {
+      setTimeout(() => {
+        element.childNodes[1].style.transform = "rotateY(0deg)";
+        document.getElementById(`${storeValue}`).style.transform = "";
+      }, 1000);
+      // document.getElementById(`card${x}`).style.transform = "";
     }
+    attempt = 0;
   }
+  attempt++;
 }
 
 function turnBack() {}
